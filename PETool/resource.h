27d3764a39546fb75eb64c46a191f68928a5e640
zkh <lovep@DESKTOP-6O9OE75>
2023-06-22 08:25:56 +0800

    [add] new pe info ui

--- PETool/resource.h
+++ PETool/resource.h
@@ -7,9 +7,31 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_PETOOL_DIALOG               102
 #define IDR_MAINFRAME                   128
+#define IDD_PEINFO_DIALOG               141
 #define IDC_EDIT_FILE_PATH              1000
+#define IDC_EDIT_OEP                    1000
 #define IDC_BUTTON_SELECT_FILE          1002
+#define IDC_EDIT_IB                     1002
 #define IDC_EDIT_PE_CONTENT             1003
+#define IDC_EDIT_SOI                    1003
+#define IDC_EDIT_BOC                    1004
+#define IDC_EDIT_BOD                    1005
+#define IDC_EDIT_SA                     1006
+#define IDC_EDIT_FA                     1007
+#define IDC_EDIT_MA                     1008
+#define IDC_EDIT_SY                     1009
+#define IDC_EDIT_NOS                    1010
+#define IDC_EDIT_TDS                    1011
+#define IDC_EDIT_SOH                    1012
+#define IDC_EDIT_DC                     1013
+#define IDC_EDIT_CS                     1014
+#define IDC_EDIT_SOOH                   1015
+#define IDC_EDIT_NORAS                  1016
+#define IDC_BUTTON_SECTIONS             1019
+#define IDC_BUTTON_TIME                 1022
+#define IDC_BUTTON8                     1026
+#define IDC_BUTTON10                    1029
+#define IDC_BUTTON7                     1048
 
 // Next default values for new objects
 // 
